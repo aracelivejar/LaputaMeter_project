83b92beafc7d5f82788fd73bd80ba89f41df7ff5
Araceli Vejar <gloriaaraceliduenasbejar@gmail.com>
2025-07-01 13:14:42 +0300

    modificacion models

--- laptm_app/models.py
+++ laptm_app/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 
-class laptm_app(models.Model):
+class Member(models.Model):
     firstname = models.CharField(max_length=255)
     lastname = models.CharField(max_length=255)
 
